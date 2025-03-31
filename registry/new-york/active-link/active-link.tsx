4523ebb20c81039c81279b1813711d117ad7b32a
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-03-31 09:33:39 +0900

    feat(active-link): propsを追加

--- registry/new-york/active-link/active-link.tsx
+++ registry/new-york/active-link/active-link.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -7,13 +9,14 @@ export function ActiveLink({
   className,
   exact,
   ref,
+  ...props
 }: {
   href: string;
   children: React.ReactNode;
   className?: string;
   exact?: boolean;
   ref?: React.Ref<HTMLAnchorElement>;
-}) {
+} & React.ComponentProps<typeof Link>) {
   const pathname = usePathname();
   const isRoot = href === "/";
   const isActive = exact
@@ -28,6 +31,7 @@ export function ActiveLink({
       aria-current={isActive ? "page" : undefined}
       className={className}
       ref={ref}
+      {...props}
     >
       {children}
     </Link>
