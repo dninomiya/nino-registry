58e949eb578bee51d33a3a8753175e28222841a7
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-03-31 09:35:07 +0900

    feat(home): Homeコンポーネントにボタンとリンクを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 import { getRegistry } from "@/lib/registry";
+import { ActiveLink } from "@/registry/new-york/active-link/active-link";
 
 export default async function Home() {
-  const registry = await getRegistry()
+  const registry = await getRegistry();
 
   return (
     <div>
@@ -10,6 +12,11 @@ export default async function Home() {
         <div key={item.name}>
           <h2>{item.name}</h2>
           <p>{item.description}</p>
+          <Button asChild variant="outline">
+            <ActiveLink href="/" className="aria-[current=page]:text-red-500">
+              Home
+            </ActiveLink>
+          </Button>
         </div>
       ))}
     </div>
