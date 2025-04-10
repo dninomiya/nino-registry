53d276dbc46351d129a9a156a0a0e0e301db8340
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-04-11 06:16:57 +0900

    fix(get-base-url): Vercelの環境変数を修正し、ドキュメントのURL例を更新しました。

--- registry/new-york/get-base-url/get-base-url.ts
+++ registry/new-york/get-base-url/get-base-url.ts
@@ -13,7 +13,7 @@ export const getBaseURL = () => {
   const isProd = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
   const url = isProd
     ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-    : process.env.NEXT_PUBLIC_VERCEL_URL;
+    : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL;
 
   return url
     ? `https://${url}`
