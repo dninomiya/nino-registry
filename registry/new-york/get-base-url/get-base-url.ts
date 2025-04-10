3a88db70808a7abbdbe1273818a7bfa98743c701
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-04-11 06:08:52 +0900

    fix(get-base-url): getBaseURL関数で環境変数のプレフィックスを修正し、NEXT_PUBLICを追加しました。

--- registry/new-york/get-base-url/get-base-url.ts
+++ registry/new-york/get-base-url/get-base-url.ts
@@ -12,8 +12,8 @@
 export const getBaseURL = () => {
   const isProd = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
   const url = isProd
-    ? process.env.VERCEL_PROJECT_PRODUCTION_URL
-    : process.env.VERCEL_URL;
+    ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+    : process.env.NEXT_PUBLIC_VERCEL_URL;
 
   return url
     ? `https://${url}`
